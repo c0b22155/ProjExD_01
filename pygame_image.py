b9efdd5551ac68b8f1dbc8f211d7c5a335798d28
yamaneki <c0b22155ef@edu.teu.ac.jp>
2023-11-21 15:58:37 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -16,13 +16,16 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0]) #練習4 背景画像の表示
+            
+        x=tmr%1600
+        #print(tmr,x)　　　　　　　　　#練習6 動く背景画像の表示
+        screen.blit(bg_img, [-x, 0]) #練習4 背景画像の表示
+        screen.blit(bg_img, [1600-x, 0]) #練習6 動く背景画像の表示
         screen.blit(kk_imgs[tmr%2], [300, 200]) #練習5　こうかとんはばたく
             
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
