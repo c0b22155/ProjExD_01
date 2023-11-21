b88942227571f9a969b733526dcc289c6d9270fa
yamaneki <c0b22155ef@edu.teu.ac.jp>
2023-11-21 16:24:28 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,8 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習１ 背景画像Surface
     kk_img = pg.image.load("ex01/fig/3.png") #練習2 こうかとん画像Surfaceの生成
     kk_img = pg.transform.flip(kk_img, True, False)#練習2 こうかとん画像の左右反転
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]#練習3　こうかとんSurfaceのリスト
+    
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0),pg.transform.rotozoom(kk_img, 20, 1.0)]#練習3　こうかとんSurfaceのリスト
     
     tmr = 0
 
@@ -17,15 +18,15 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
             
-        x=tmr%1600
+        x=tmr%1600                   
         #print(tmr,x)　　　　　　　　　#練習6 動く背景画像の表示
         screen.blit(bg_img, [-x, 0]) #練習4 背景画像の表示
-        screen.blit(bg_img, [1600-x, 0]) #練習6 動く背景画像の表示
-        screen.blit(kk_imgs[tmr%2], [300, 200]) #練習5　こうかとんはばたく
+        screen.blit(bg_img, [1600-x, 0]) #練習6 動く背景画像の表示 
+        screen.blit(kk_imgs[tmr%3], [300, 200]) #練習5　こうかとんはばたく
             
         pg.display.update()
-        tmr += 1        
-        clock.tick(100)
+        tmr += 1       
+        clock.tick(400)
 
 
 if __name__ == "__main__":
